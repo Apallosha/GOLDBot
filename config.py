ba9c36241c18acc3b9f1a14cde499c493df6ff4e
Apallosha <artemfigurskiy444@gmail.com>
2026-01-20 19:29:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 TOKEN = "8360081661:AAEOoOdf5tLSfr2scMebO9mY-iq2jzMHrB8"
-BOT_USERNAME = "ApalloshaGOLD_Bot"  # без @
-ADMIN_IDS = [5333130126]  # ваш числовой ID
-PROFILE_PHOTO = "https://files.catbox.moe/r039wr.jpg"
-REF_BONUS = 2
-MIN_WITHDRAW = 30
+
 WEBHOOK_URL = "https://goldbot-8euq.onrender.com"
+
+ADMIN_IDS = [5333130126]  # ТВОЙ telegram id
+
+PROFILE_PHOTO = "https://files.catbox.moe/r039wr.jpg"
+
 DATABASE = "database.db"
