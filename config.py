f99678eadff1717fc06e3c64a1735a35a1a0cb3d
Apallosha <artemfigurskiy444@gmail.com>
2026-01-20 18:45:35 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 TOKEN = "8360081661:AAEOoOdf5tLSfr2scMebO9mY-iq2jzMHrB8"
-ADMIN_IDS = [5333130126]  # пример: [123456789]
-BOT_USERNAME = "ApalloshaGOLD_Bot"     # имя бота без @
-PROFILE_PHOTO = "https://files.catbox.moe/r039wr.jpg"  # ссылка на фото
-
-# Экономика
+BOT_USERNAME = "ApalloshaGOLD_Bot"  # без @
+ADMIN_IDS = [5333130126]  # ваш числовой ID
+PROFILE_PHOTO = "https://files.catbox.moe/r039wr.jpg"
 REF_BONUS = 2
 MIN_WITHDRAW = 30
+WEBHOOK_URL = "https://goldbot-8euq.onrender.com"
+DATABASE = "database.db"
