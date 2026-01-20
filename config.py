de0a51ee75ebe8136e148af29f8ef14430aeb3cf
Apallosha <artemfigurskiy444@gmail.com>
2026-01-20 17:10:48 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN "8360081661:AAEOoOdf5tLSfr2scMebO9mY-iq2jzMHrB8"
+
+# ID администратора (можно несколько)
+ADMIN_IDS = [5333130126]
+
+BOT_USERNAME = "ApalloshaGOLD_Bot"  # без @
+
+# Экономика
+REF_BONUS = 2
+MIN_WITHDRAW = 30
+
+# Фото профиля (одна картинка для всех)
+PROFILE_PHOTO = "https://files.catbox.moe/r039wr.jpg"  # замени на своё фото
